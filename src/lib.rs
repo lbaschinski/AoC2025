2b0dcebb387dd87e099645e5ea49be75df058c0c
Lucas Grosche <lucas.grosche@cyberus-technology.de>
2025-12-19 12:12:39 +0100

    Lib: add parse_string_to_i64 function

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,12 @@ use anyhow::{Result, anyhow};
 
 pub fn split_range(range: String) -> Result<(i64, i64)> {
     let mut values = range.split('-');
-    let start = values.next().ok_or(anyhow!("no start value in this range"))?.trim().parse::<i64>()?;
-    let end = values.next().ok_or(anyhow!("no end value in this range"))?.trim().parse::<i64>()?;
+    let start = parse_string_to_i64(values.next().ok_or(anyhow!("no start value in this range"))?)?;
+    let end = parse_string_to_i64(values.next().ok_or(anyhow!("no end value in this range"))?)?;
     Ok((start, end))
+}
+
+pub fn parse_string_to_i64(s: &str) -> Result<i64> {
+    s.trim().parse::<i64>()
+        .map_err(|e| anyhow!("failed to parse '{s}': {e}"))
 }
